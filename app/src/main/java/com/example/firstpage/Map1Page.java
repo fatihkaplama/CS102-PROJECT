e5743e25400c5db66bdf2442c1a60583793fe8dd
fatihkaplama <fatih.kaplama@ug.bilkent.edu.tr>
2020-05-04 23:28:24 +0300

    update

--- app/src/main/java/com/example/firstpage/Map1Page.java
+++ app/src/main/java/com/example/firstpage/Map1Page.java
@@ -21,11 +21,16 @@ public class Map1Page extends AppCompatActivity {
     Button back;
     Button info;
     Button up;
+    Button left;
+    Button right;
     LinearLayout layout;
     LinearLayout.LayoutParams params;
     Drawable upLogo;
     float x;
     float y;
+    int forwardCount = 1;
+    int leftCount = 1;
+    int rightCount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,21 +47,62 @@ public class Map1Page extends AppCompatActivity {
         layout = findViewById(R.id.linearLayout3);
         params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         up = new Button(this);
+        left = new Button(this);
+        right = new Button(this);
         up.setText("GO FORWARD");
         up.setBackgroundColor(Color.CYAN);
+        left.setText("TURN LEFT");
+        left.setBackgroundColor(Color.CYAN);
+        right.setText("TURN RIGHT");
+        right.setBackgroundColor(Color.CYAN);
 
         goForward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                layout.addView(up,params);
-                //bee.setTranslationX(100);
 
+                if (forwardCount == 1){
+                    layout.addView(up,params);
+                    GoForward();
+                    forwardCount++;
+                }
+                else {
+                    GoForward();
+                }
+
+            }
+        });
+
+        turnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (leftCount == 1){
+                    layout.addView(left,params);
+                    TurnLeft();
+                    leftCount++;
+                }
+                else {
+                    TurnLeft();
+                }
+            }
+        });
+
+        turnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rightCount == 1){
+                    layout.addView(right,params);
+                    TurnRight();
+                    rightCount++;
+                }
+                else {
+                    TurnRight();
+                }
             }
         });
     }
     public void GoForward(){
         if (bee.getRotation() == 0){
-            y -= 200;
+            y -= 180;
             bee.setTranslationY(y);
         }
 
@@ -66,13 +112,23 @@ public class Map1Page extends AppCompatActivity {
         }
 
         if (bee.getRotation() == 180){
-            y += 200;
+            y += 180;
             bee.setTranslationY(y);
         }
 
-        if (bee.getRotation() == 270){
+        if (bee.getRotation() == -90){
             x -= 200;
             bee.setTranslationX(x);
         }
+    }
+
+    public void TurnLeft(){
+
+        bee.setRotation(bee.getRotation() - 90);
+    }
+
+    public void TurnRight(){
+
+        bee.setRotation((bee.getRotation() + 90));
     }
 }
