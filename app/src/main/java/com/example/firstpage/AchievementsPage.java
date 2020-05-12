6989d42c6849662422ff98126fd6832b8cd4ecde
ArmanEnginSUCU-afk <63503839+ArmanEnginSUCU-afk@users.noreply.github.com>
2020-05-12 18:43:50 +0300

    new achievements

---
+++ app/src/main/java/com/example/firstpage/AchievementsPage.java
@@ -0,0 +1,92 @@
+package com.example.firstpage;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatDrawableManager;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.softmoore.android.graphlib.Function;
+import com.softmoore.android.graphlib.Graph;
+import com.softmoore.android.graphlib.GraphView;
+import com.softmoore.android.graphlib.Label;
+import com.softmoore.android.graphlib.Point;
+
+public class AchievementsPage extends AppCompatActivity {
+    private Button volume;
+    private boolean isVolumeOn;
+    private Drawable volumeoff;
+    private Drawable volumeon;
+    private int volumeoffID;
+    private int volumeonID;
+    private Button back;
+    private String userName;
+    private int avatarId;
+    private TextView tv;
+    private int background;
+    private ConstraintLayout AchievementsPageLayout;
+
+
+
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements_page);
+        Intent i = getIntent();
+        AchievementsPageLayout = findViewById(R.id.achievements_page_layout);
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.start();
+        background = getSharedPreferences("ShareTheme",MODE_PRIVATE).getInt("theme",0);
+        AchievementsPageLayout.setBackgroundResource(background);
+        userName = i.getStringExtra("nickname");
+        tv = findViewById(R.id.userName);
+        tv.setText(userName);
+        volume = findViewById(R.id.achievementsPage_voice_button);
+        back = findViewById(R.id.achievementsPage_back_button);
+        volumeonID = R.drawable.volumeon;
+        volumeoffID = R.drawable.volumeoff;
+        volumeon = AppCompatDrawableManager.get().getDrawable(this, volumeonID);
+        volumeoff = AppCompatDrawableManager.get().getDrawable(this, volumeoffID);
+
+        Graph graph = new Graph.Builder()
+                .build();
+        GraphView graphView = findViewById(R.id.graph_view);
+        graphView.setGraph(graph);
+        setTitle("Empty Graph");
+        TextView textView = findViewById(R.id.graph_view_label);
+        textView.setText("Graph of Axes");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AchievementsPage.this , PlayPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        volume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isVolumeOn) {
+                    volume.setBackground(volumeoff);
+                    isVolumeOn = false;
+                    mediaPlayer.pause();
+                } else {
+                    volume.setBackground(volumeon);
+                    isVolumeOn = true;
+                    mediaPlayer.start();
+                }
+            }
+        });
+    }
+}
